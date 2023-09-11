0caf0d3aa224ec4cf2167d30b066e41bb7d57442
JhoanEstebanSG <jhoan011989@gmail.com>
2023-09-11 09:58:56 -0500

    Configuracion inicial del proyecto para la conexion con los frameworks y la base de datos

--- src/main/java/co/edu/uniquindio/proyectoclinica/ProjectApplication.java
+++ src/main/java/co/edu/uniquindio/proyectoclinica/ProjectApplication.java
@@ -1,2 +1,10 @@
-package co.edu.uniquindio.proyectoclinica;public class ProjectApplication {
+package co.edu.uniquindio.proyectoclinica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ProjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectApplication.class, args);
+    }
 }
