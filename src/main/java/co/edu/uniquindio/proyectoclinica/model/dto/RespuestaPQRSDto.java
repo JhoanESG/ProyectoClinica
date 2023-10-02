e6b7dca5cec60f64a8c762f9885eaaccc3a0567c
Santiago Ramon <santiramon967gmail.com>
2023-10-02 09:21:26 -0500

    Solucion de errores

--- src/main/java/co/edu/uniquindio/proyectoclinica/model/dto/RespuestaPQRSDto.java
+++ src/main/java/co/edu/uniquindio/proyectoclinica/model/dto/RespuestaPQRSDto.java
@@ -5,7 +5,7 @@ import co.edu.uniquindio.proyectoclinica.model.entities.Usuario;
 import java.time.LocalDateTime;
 
 public record RespuestaPQRSDto(
-        String codigo,
+        int codigo,
         String descripcion,
 
         Usuario usuario
