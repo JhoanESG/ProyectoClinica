7677105b45d77c4fcd52f7fd858bd03f4576eab6
Santiago Ramon <santiramon967gmail.com>
2023-11-19 23:21:53 -0500

    Se implemento el servicio de listar medicos

---
+++ src/main/java/co/edu/uniquindio/proyectoclinica/model/dto/ItemMedicoDto.java
@@ -0,0 +1,7 @@
+package co.edu.uniquindio.proyectoclinica.model.dto;
+
+public record ItemMedicoDto(
+        String nombre,
+        String cedula
+) {
+}
--- src/main/java/co/edu/uniquindio/proyectoclinica/repositorios/MedicoRepositorio.java
+++ src/main/java/co/edu/uniquindio/proyectoclinica/repositorios/MedicoRepositorio.java
@@ -1,8 +1,11 @@
 package co.edu.uniquindio.proyectoclinica.repositorios;
 
 import co.edu.uniquindio.proyectoclinica.model.entities.Medico;
+import co.edu.uniquindio.proyectoclinica.model.enums.Especialidad;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MedicoRepositorio extends JpaRepository<Medico,String> {
@@ -12,4 +15,6 @@ public interface MedicoRepositorio extends JpaRepository<Medico,String> {
 
     //@Query("select m from medico m where m.cedula = :cedula")
     Medico findByCedula(String cedula);
+
+    List<Medico> findByEspecialidad(Especialidad especialidad);
 }
